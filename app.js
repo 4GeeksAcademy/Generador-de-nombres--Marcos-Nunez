6375617f088cc718a7fa4cf7b50958cddeadb31f
nunezdelgadomarcos <nunezdelgadomarcos@gmail.com>
2024-11-06 20:24:18 +0000

    second commit

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 
-let dominio = pronoun.flatMap((p) =>
-  adj.flatMap((a) => noun.map((n) => `${p}${a}${n}.com`))
+let dominio = pronoun.flatMap((pronombre) =>
+  adj.flatMap((adjetivo) => noun.map((nombre) => `${pronombre}${adjetivo}${nombre}.com`))
 );
 
 console.log(dominio);
